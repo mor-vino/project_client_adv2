0f530515353b0ac9e8de5ed7a0c2d70412e56641
tomer <shaltomer@gmail.com>
2015-08-09 16:57:34 +0300

    Documentation until "JoinChannel" class

--- app/src/main/java/com/example/mor/final_project_client_adv2/GetUpdatesFromServer.java
+++ app/src/main/java/com/example/mor/final_project_client_adv2/GetUpdatesFromServer.java
@@ -4,26 +4,48 @@ import android.app.Activity;
 
 /**
  * Created by mor on 08/08/2015.
+ * connect to the server
  */
 public class GetUpdatesFromServer {
     private Activity myActivity;
     private String app_Id;
 
+    /**
+     * constructor
+     * @param act current activity
+     * @param appId the id of the app
+     */
     public GetUpdatesFromServer (Activity act, String appId){
         myActivity = act;
         app_Id = appId;
     }
+
+    /**
+     * sync all the update
+     */
     public void syncAllUpdates(){
         syncMyChannelsUpdates();
         syncAllChannelsUpdates();
         syncAllChannelsMembersUpdates();
     }
+
+    /**
+     * sync the current channel
+     */
     public void syncMyChannelsUpdates(){
         new GetMyChannels(myActivity).execute("http://" + app_Id + ".appspot.com/getMyChannels");
     }
+
+    /**
+     * sync all the channel updates
+     */
     public void syncAllChannelsUpdates(){
         new GetAllChannels(myActivity).execute("http://" + app_Id + ".appspot.com/getChannels");
     }
+
+    /**
+     * sync the channel members
+     */
     public void syncAllChannelsMembersUpdates(){
         new GetNetwork(myActivity).execute("http://" + app_Id + ".appspot.com/getNetwork");
     }
